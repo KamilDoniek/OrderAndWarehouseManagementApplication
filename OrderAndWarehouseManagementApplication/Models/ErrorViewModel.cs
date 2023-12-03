6a673c203fdebc48fcc8a8541a7b6b9a35cccb79
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2023-12-03 16:14:32 +0100

    first commit

---
+++ OrderAndWarehouseManagementApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OrderAndWarehouseManagementApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
